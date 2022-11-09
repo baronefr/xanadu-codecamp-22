03e41c199744df0c082a7a6002155e28684172f4
baronefr <baronefr7@gmail.com>
2022-11-10 00:36:13 +0100

    add ex 7 10

---
+++ 10_universality_CNOT-U3.py
@@ -0,0 +1,79 @@
+import functools
+import json
+import math
+import pandas as pd
+import pennylane as qml
+import pennylane.numpy as np
+import scipy
+
+########################### our code ###############################
+
+def circuit():
+    """
+    Succession of gates that will generate the requested matrix.
+    This function does not receive any arguments nor does it return any values.
+    """
+
+    qml.U3(-np.pi/2, 0, 0, wires=0)
+    qml.CNOT(wires=(1, 0))
+    qml.U3(np.pi/2, 0, 0, wires=0)
+    qml.U3(-np.pi/2, 0, 0, wires=2)
+    qml.U3(0, np.pi, 0, wires=2)
+
+####################################################################
+
+
+# These functions are responsible for testing the solution.
+
+def run(input: str) -> str:
+    matrix = qml.matrix(circuit)().real
+
+    with qml.tape.QuantumTape() as tape:
+        circuit()
+
+    names = [op.name for op in tape.operations]
+    return json.dumps({"matrix": matrix.tolist(), "gates": names})
+
+def check(user_output: str, expected_output: str) -> str:
+    parsed_output = json.loads(user_output)
+    matrix_user = np.array(parsed_output["matrix"])
+    gates = parsed_output["gates"]
+
+    solution = (
+        1
+        / np.sqrt(2)
+        * np.array(
+            [
+                [1, 1, 0, 0, 0, 0, 0, 0],
+                [1, -1, 0, 0, 0, 0, 0, 0],
+                [0, 0, -1, -1, 0, 0, 0, 0],
+                [0, 0, -1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 0, 0],
+                [0, 0, 0, 0, 1, -1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 1],
+                [0, 0, 0, 0, 0, 0, 1, -1],
+            ]
+        )
+    )
+
+    assert np.allclose(matrix_user, solution)
+    assert len(set(gates)) == 2 and "U3" in gates and "CNOT" in gates
+
+
+test_cases = [['No input', 'No output']]
+
+for i, (input_, expected_output) in enumerate(test_cases):
+    print(f"Running test case {i} with input '{input_}'...")
+
+    try:
+        output = run(input_)
+
+    except Exception as exc:
+        print(f"Runtime Error. {exc}")
+
+    else:
+        if message := check(output, expected_output):
+            print(f"Wrong Answer. Have: '{output}'. Want: '{expected_output}'.")
+
+        else:
+            print("Correct!")
